93bf4c00e711793011f97619dd35da8e327de0a2
elliotmoose <elliot_koh_1997@yahoo.com.sg>
2018-04-28 22:33:48 +0800

    added claiming of discounts

--- server_main.js
+++ server_main.js
@@ -3,10 +3,13 @@ const bodyParse = require('body-parser')
 const path = require('path')
 const app = express()
 const mysql = require('mysql');
+const fs = require('fs');
+
+var con = ConnectDatabase();
 
 
 
-
+//#region express
 app.use((request, response, next) => {
   //console.log(request.headers)
   next()
@@ -16,6 +19,8 @@ app.use(bodyParse.json())
 app.use(bodyParse.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, "public")))
 app.use('/scripts', express.static(__dirname + '/node_modules'));
+//#endregion
+
 
 
 // Handles request to root only.
@@ -25,6 +30,8 @@ app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
+
+//#region Get Generic Data 
 //query functions
 app.get('/GetBarNames', (req, res) => {
   QueryDB("SELECT * FROM bars_info").then(function (data) {
@@ -42,6 +49,24 @@ app.get('/GetDiscounts', (req, res) => {
   });
 });
 
+app.get('/GetImageForBar/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (id == undefined) {
+    Output(false, "No bar id specified", res);
+  }
+
+  var imagePath = path.resolve(__dirname, "bar_images/" + id + "/0.jpg");
+
+  if (fs.existsSync(imagePath)) {
+    res.sendFile(imagePath);
+  }
+  else {
+    Output(false,`image does not exist for this id ${id}`,res);
+  }
+});
+
+//#endregion
 //action functions
 app.post('/GenerateUser', (req, res) => {
   var id = req.body.uuid;
@@ -90,6 +115,29 @@ app.post('/AddDiscountToWalletForUser', (req, res) => {
   }
 });
 
+app.post('/AddDiscountClaim', (req, res) => {
+  var user_id = req.body.user_id;
+  var discount_id = req.body.discount_id;
+
+  if (user_id == undefined || discount_id == undefined) {
+    Output(false, "No id specified", res);
+    return;
+  }
+  else {
+    var epoch = Math.round(new Date().getTime() / 1000);
+    var queryString = `INSERT INTO discount_claims (id,discount_id,user_id,date) values (0,${discount_id},${user_id},${epoch})`;
+    
+    QueryDB(queryString).then(function (data) {
+      Output(true, data, res);
+    }).catch(function (err) {
+      Output(false, err, res);
+    });
+  }
+
+  
+});
+
+
 app.post('/GetWalletForUser', (req, res) => {
   var id = req.body.id;
   if (id == undefined) {
@@ -99,8 +147,8 @@ app.post('/GetWalletForUser', (req, res) => {
   else {
     var walletOutput;
     QueryDB("SELECT wallet FROM users WHERE id='" + id + "'").then(function (walletData) {
-      walletOutput = JSON.parse(walletData[0].wallet);      
-    }).then(function(){
+      walletOutput = JSON.parse(walletData[0].wallet);
+    }).then(function () {
       Output(true, walletOutput, res);
       return;
     }).catch(function (err) {
@@ -108,6 +156,9 @@ app.post('/GetWalletForUser', (req, res) => {
     });
   }
 });
+
+
+
 
 app.listen(8080)
 
@@ -120,49 +171,43 @@ function ConnectDatabase() {
     database: "afterdarksg"
   });
 
-  //   con.connect(function(err) {
-  //     if (err) throw err;
-  //     console.log("Connected!");
-  //   });
+    con.connect(function(err) {
+      if (err) throw err;
+      console.log("Connected to database");
+    });
+  // con.connect(function (err) {
 
+  //   if (err) {
+  //     Output(false, err, res);
+  //     return;
+  //   }
+  // });
   return con;
 }
 
-function Query(query, res) {
-  var con = ConnectDatabase()
-  con.connect(function (err) {
+// function Query(query, res) {
 
-    if (err) {
-      Output(false, err, res);
-      return;
-    }
+//     con.query(query, function (err, result, fields) {
 
-    con.query(query, function (err, result, fields) {
+//       if (err) {
+//         Output(false, err, res);
+//         return;
+//       }
 
-      if (err) {
-        Output(false, err, res);
-        return;
-      }
-
-      Output(true, result, res);
-      return;
-    });
-  });
-}
+//       Output(true, result, res);
+//       return;
+//     });
+ 
+// }
 
 function QueryDB(query) {
   return new Promise(function (resolve, reject) {
-    var con = ConnectDatabase()
-    con.connect(function (err) {
+    
+    con.query(query, function (err, result, fields) {
       if (err) {
         reject(err);
       }
-      con.query(query, function (err, result, fields) {
-        if (err) {
-          reject(err);
-        }
-        resolve(result);
-      });
+      resolve(result);
     });
   });
 }
