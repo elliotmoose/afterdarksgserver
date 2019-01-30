26e18a67d4afdf1a65f7154bb03b37bdcd9bcd00
elliotmoose <elliot_koh_1997@yahoo.com.sg>
2019-01-30 16:55:34 +0800

    config file, with https

---
+++ config.js
@@ -0,0 +1,11 @@
+const configs = {
+    https : false,
+    port: 80,
+    SSLPORT: 443,
+    domain: 'afterdarksg.com'
+}
+
+configs.SSL_PK_PATH = `/etc/letsencrypt/live/${configs.domain}/privkey.pem`;
+configs.SSL_CERT_PATH = `/etc/letsencrypt/live/${configs.domain}/cert.pem`;
+
+module.exports.configs = configs
--- server_main.js
+++ server_main.js
@@ -2,9 +2,73 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const api = require('./api');
+const config = require('./config');
+var https = require('https');
 
 app.use('/api',api);
 app.use(express.static(path.join(__dirname, "public")));
 app.use('/scripts', express.static(__dirname + '/node_modules'));
 
 app.listen(80);
+
+
+const domain = 'afterdarksg.com';
+//Create server over HTTPS
+const configs = {    
+    
+}
+
+// https.createServer(options, app)
+//   .listen(80, function () {
+//     console.log('Example app listening on port 80! Go to https://localhost/')
+//   })
+
+// //Redirect HTTP connections to HTTPS server
+// http.createServer(function (req, res) {
+//     res.writeHead(301, {
+//         "Location": "https://" + req.headers['host'] + req.url
+//     });
+//     res.end();
+// }).listen(config.port);
+
+//Start the server over HTTPS
+// var server = https.createServer(options, app).listen(config.SSLPORT, function () {
+//     var host = server.address().address;
+//     var port = server.address().port;
+//     console.log("%s host listening on port %s", host, port);
+// });
+
+
+if (config.https) {
+    //Create server over HTTPS
+    var fs = require('fs');
+    var privateKey = fs.readFileSync(config.SSL_PK_PATH);
+    var certificate = fs.readFileSync(config.SSL_CERT_PATH);
+    var options = {
+        key: privateKey,
+        cert: certificate
+    };
+
+    //Redirect HTTP connections to HTTPS server
+    http.createServer(function (req, res) {
+        res.writeHead(301, {
+            'Location': 'https://' + req.headers['host'] + req.url
+        });
+        res.end();
+    }).listen(config.port);
+
+    //Start the server over HTTPS
+    var server = https.createServer(options, app).listen(config.SSLPORT, function () {
+        var host = server.address().address;
+        var port = server.address().port;
+        console.log('%s host listening on port %s', host, port);
+    });
+} else {
+
+    //Start the server over HTTP
+    var http_server = app.listen(config.port, function () {
+        var host = http_server.address().address;
+        var port = http_server.address().port;
+        console.log('%s host listening on port %s', host, port);
+    });
+}
